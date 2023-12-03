1f251c6850ed07c1eda4d591cd2a36704c5cf053
Aldo Mateli <aldo.mateli@gmail.com>
2023-12-03 14:29:52 +0000

    Refactor day3

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,55 +1,5 @@
-import java.io.File
-import kotlin.text.StringBuilder
-
-/**
- * Given a star location, find all digits around it and return their product
- * only if there's exactly two of them.
- */
-fun findGearRatio(input: MutableList<MutableList<Char>>, y: Int, x: Int): Int {
-    val parts: List<Int> = mutableListOf()
-
-    for (dx in -1..1) {
-        for (dy in -1..1) {
-            val xx = x + dx
-            val yy = y + dy
-
-            if (yy in input.indices && xx in input[y].indices && input[yy][xx].isDigit()) {
-                val chars = StringBuilder()
-                var cx = xx
-
-                while (cx >= 0 && input[yy][cx].isDigit()) {
-                    chars.append(input[yy][cx])
-                    input[yy][cx] = '.'
-                    cx -= 1
-                }
-
-                chars.reverse()
-                cx = xx + 1
-
-                while (cx < input[yy].size && input[yy][cx].isDigit()) {
-                    chars.append(input[yy][cx])
-                    input[yy][cx] = '.'
-                    cx += 1
-                }
-
-                parts.addLast(chars.toString().toInt())
-
-                if (parts.size > 2) {
-                    return 0
-                }
-            }
-        }
-    }
-
-    return if (parts.size == 2) {
-        parts[0] * parts[1]
-    } else {
-        0
-    }
-}
-
-fun isSymbol(c: Char) = !(c.isDigit() || c == '.')
-
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
 
 /**
  * For part 1, find numbers and for every digit added to the number
@@ -60,6 +10,7 @@ fun getPart1Nums(input: List<String>): List<Int> {
     val res: List<Int> = mutableListOf()
     val current = StringBuilder()
     var symbolSeen = false
+    val isSymbol: (Char) -> Boolean = { !(it == '.' || it.isDigit()) }
 
     for (y in input.indices) {
         for (x in input[y].indices) {
@@ -68,7 +19,6 @@ fun getPart1Nums(input: List<String>): List<Int> {
             if (char == '.' || !char.isDigit()) {
                 if (symbolSeen && current.isNotEmpty()) {
                     res.addLast(current.toString().toInt())
-                    current.clear()
                 }
 
                 current.clear()
@@ -101,27 +51,69 @@ fun getPart1Nums(input: List<String>): List<Int> {
     return res
 }
 
-/**
- * Part 2 takes the opposite approach to part 1, for every star find all digits around it.
- */
-fun getPart2Nums(input: List<String>): Int {
-    var sum = 0
-    val mutableInput = input.map { it.toMutableList() }.toMutableList()
 
-    for (y in input.indices) {
-        for (x in input[y].indices) {
-            if (input[y][x] == '*') {
-                sum += findGearRatio(mutableInput, y = y, x = x)
+/**
+ * Given a star location, find all digits around it and return their product
+ * only if there's exactly two of them.
+ */
+fun findGearRatio(input: MutableList<MutableList<Char>>, y: Int, x: Int): Int {
+    if (input[y][x] != '*') {
+        return 0
+    }
+
+    val parts: List<Int> = mutableListOf()
+
+    for (dx in -1..1) {
+        for (dy in -1..1) {
+            val xx = x + dx
+            val yy = y + dy
+
+            if (yy in input.indices && xx in input[y].indices && input[yy][xx].isDigit()) {
+                val chars = buildString {
+                    var cx = xx
+                    while (cx >= 0 && input[yy][cx].isDigit()) {
+                        append(input[yy][cx])
+                        input[yy][cx] = '.'
+                        cx -= 1
+                    }
+
+                    reverse()
+
+                    cx = xx + 1
+                    while (cx < input[yy].size && input[yy][cx].isDigit()) {
+                        append(input[yy][cx])
+                        input[yy][cx] = '.'
+                        cx += 1
+                    }
+                }
+
+                parts.addLast(chars.toInt())
+
+                if (parts.size > 2) {
+                    return 0
+                }
             }
         }
     }
 
-    return sum
+    return when (parts.size) {
+        2 -> parts[0] * parts[1]
+        else -> 0
+    }
+}
+
+/**
+ * Part 2 takes the opposite approach to part 1, for every star find all digits around it.
+ */
+fun getPart2Nums(input: MutableList<MutableList<Char>>): Int {
+    return input.flatMapIndexed { y, row ->
+        List(row.size) { x -> findGearRatio(input, y, x) }
+    }.sum()
 }
 
 fun main(args: Array<String>) {
-    val lines = File(args[0]).readLines()
+    val lines = Path(args[0]).readLines()
 
     println(getPart1Nums(lines).sum())
-    println(getPart2Nums(lines))
+    println(getPart2Nums(lines.map { it.toMutableList() }.toMutableList()))
 }
